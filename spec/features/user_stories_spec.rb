93418c67eb9b5604903c1679b9d91cb89446538b
RussellWind <russell.wind@gmail.com>
2019-08-06 16:48:18 +0100

    players can enter names and they are displayed

--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -1,6 +1,14 @@
 describe 'battle web app', type: :feature do
-  it 'infrastructure working' do
+  # it 'infrastructure working' do
+  #   visit '/'
+  #   expect(page).to have_content 'Testing infrastructure working!'
+  # end
+
+  it 'players can enter names and they are displayed' do
     visit '/'
-    expect(page).to have_content 'Testing infrastructure working!'
+    fill_in 'player_one_name', with: 'Andy'
+    fill_in 'player_two_name', with: 'Beatrice'
+    click_button 'Submit'
+    expect(page).to have_content 'Andy'
   end
 end
